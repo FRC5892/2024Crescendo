37f0b730e2db11d0a86587fae252e1a4260d198b
Garby <117385299+MichaelStatt-S@users.noreply.github.com>
2024-02-10 17:43:26 -0600

    no more compiler errors!!

--- src/main/java/frc/robot/subsystems/GroundIntake.java
+++ src/main/java/frc/robot/subsystems/GroundIntake.java
@@ -11,6 +11,7 @@ import com.revrobotics.CANSparkBase.ControlType;
 
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib.HeroSparkPID;
 import frc.robot.Constants;
@@ -123,6 +124,9 @@ public class GroundIntake extends SubsystemBase {
     if (intakeDeployed) {
       deployMotor.set(IntakeConstants.retractSpeed);
     }
+  }
+    public Command outtakeNoteCommand () {
+    return startEnd(() -> this.outtakeNote(), ()-> this.stopIntake());
   }
 
   // public void retractIntake() {
