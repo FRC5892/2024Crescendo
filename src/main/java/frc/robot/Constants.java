b5227255d9d7acb4f2c5cc359cc60e66f8858827
Emiliano Lane <s1658059@online.houstonisd.org>
2024-03-02 10:12:52 -0600

    update climb ids

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -24,12 +24,12 @@ public final class Constants {
 
   public static final class ClimbConstants {
     public static final int elevatorMotorID = 40;
-    public static final int leftClimbMotorID = 41;
-    public static final int rightClimbMotorID = 42;
+    public static final int leftClimbMotorID = 19;
+    public static final int rightClimbMotorID = 20;
     public static final double elevatorSpeed = 0.5;
     public static final double climbSpeed = 0.5;
     public static double rotations = 40;
-    public static final int intakeElevatorID = 44;
+    public static final int intakeElevatorID = 41;
   }
 
   public static final class IntakeConstants {
